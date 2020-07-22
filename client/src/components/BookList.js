1329b357b715c3610b740d7ecc3ca65de33640e4
Sankar Martha <sankarmartha.123@gmail.com>
2020-07-22 22:53:22 +0530

    added mutation in react

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,13 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {graphql} from 'react-apollo';
+import {getBooksQuery} from '../components/queries/queries'
 
-function BookList() {
-  return (
-    <div id="book-list">
-      <ui id="book-list">
-          <li>Book name</li>
-      </ui>
-    </div>
-  );
+
+// function BookList() {
+//   return (
+//     <div id="book-list">
+//     { console.log(props)}
+//       <ui id="book-list">
+//           <li>Book name</li>
+//       </ui>
+//     </div>
+//   );
+// }
+
+class BookList extends Component {
+  displayBooks(){
+    var data = this.props.data;
+    if(data.loading){
+      return (<div>Loading books...</div>)
+    }else{
+      return data.books.map(book => {
+        return(
+          <li key={ book.id }>{book.name}</li>
+        );
+      })
+    }
+  }
+  render(){
+      // console.log(this.props)
+    return (
+      <div id="book-list">
+        <ul id="book-list">
+            {this.displayBooks()}
+        </ul>
+      </div>
+    );
+  }
 }
 
-export default BookList;
+export default graphql(getBooksQuery)(BookList);
